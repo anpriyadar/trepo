ac416d0ae6ff1573f1d8bb32b0ca9b8851e7fa27
Animesh Priyadarshi <animeshpriyadarshi@Animeshs-MacBook-Pro.local>
2022-07-27 21:59:46 +0900

    test

---
+++ src/stories/HeaderTabs.stories.tsx
@@ -0,0 +1,11 @@
+import * as React from "react"
+import { Button } from "@material-ui/core"
+
+export const ButtonExample: VFC = () => {
+return (
+<div><button>
+{" "}
+Click Me!{" "}
+</button></div>
+)
+}
